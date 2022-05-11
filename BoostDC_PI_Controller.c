2e31254761ba8fe471f14295b582d3bb8b5455cd
Serena <39934557+ctrinh01028485@users.noreply.github.com>
2022-05-11 14:56:03 +0700

    Final update

--- BoostDC_PI_Controller.c
+++ BoostDC_PI_Controller.c
@@ -2,17 +2,17 @@
 
 #define kp 0.1
 #define ki 0.01
-#define Ts 0.001
+#define Ts 0.001       // Sample time
 
 double error;
 double integral;
 double D;
+double voltFB;
+double volt;
 
 ISR (TIMER2_COMPA_vect){        //TIMER2_OVF_vect
     double setpoint = 12;
     double result;
-    double voltFB;
-    double volt;
 
     volt = analogRead(A3);
     voltFB = volt * 5 * 5 / 1023;
@@ -28,37 +28,37 @@ ISR (TIMER2_COMPA_vect){        //TIMER2_OVF_vect
         result = 0;
 
     D = result;
-    //digitalWrite(12,!digitalRead(12)); //Sáng tắt LED0
+    //digitalWrite(12,!digitalRead(12));
 }
 
-
 void setup(){ 
-    //Serial.begin(9600);
+    Serial.begin(9600);
 
-    // Ngat Timer 2
+    // Timer 2 interrupt with sample time Ts = 0.001s (frequency 1000Hz) 
     TCCR2A = 0;
     TCCR2B = 0;
     TCNT2 = 0;
 
-    OCR2A = 15;               // 1000.xxx Hz = (16000000/((OCR2A +1)*1024))
+    OCR2A = 15;               // ~ 1000 Hz = (16000000/((OCR2A + 1)*1024))
     TCCR2A |= (1 << WGM21);   // CTC
     TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);  // Prescaler 1024
     TIMSK2 |= (1 << OCIE2A);  // Output Compare Match A Interrupt Enable
 
-    sei(); //Cho phép ngắt toàn cục   
+    sei(); // Enable global interrupts   
 
-    TCCR1A=0; TCCR1B=0; // RESET lại 2 thanh ghi
-    DDRB |= (1 << PB2); // Đầu ra PB2 là OUTPUT ( pin 10)                 
+    // PWM by timer 1 with frequency 20kHz
+    TCCR1A = 0; TCCR1B = 0; // Reset 2 registers
+    DDRB |= (1 << PB2); // PB2 is OUTPUT ( pin 10)                 
     
     TCCR1A |= (1 << WGM11);
-    TCCR1B |= (1 << WGM12)|(1 << WGM13); // chọn Fast PWM chế độ chọn TOP_value tự do  ICR1                     
-    TCCR1A |= (1 << COM1B1); // So sánh thường( none-inverting)
-    ICR1 = 800;              // xung răng cưa tràn sau 800 P_clock
-    TCCR1B |= (1 << CS10);   // F_clock/1=16mhz
+    TCCR1B |= (1 << WGM12)|(1 << WGM13); // select Fast PWM mode select TOP_value freely ICR1                     
+    TCCR1A |= (1 << COM1B1); // None-inverted mode
+    ICR1 = 800;              // Sawtooth pulse overflow after 800 clocks or Top_value = 800
+    TCCR1B |= (1 << CS10);   // No Prescaling = F_Clock or F_clock/1=16mhz
     //F_pwm = 16mhz/800 = 20kHz
 }
 
-
 void loop(){
-    OCR1B = D*800;           // PWM D%
+    OCR1B = D*800;           // Duty cycle D%
+    Serial.println(voltFB);
 }
