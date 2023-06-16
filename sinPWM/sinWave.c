5c445a7fc44d3fdfc7c926a74bc41a4d34591c24
Serena <39934557+ctrinh01028485@users.noreply.github.com>
2023-06-17 00:36:13 +0700

    Update sinWave.c

--- sinPWM/sinWave.c
+++ sinPWM/sinWave.c
@@ -4,17 +4,18 @@
 
 #define pi 3.1415926535897932384626433832795
 #define F 50.0
-#define M
+#define M 1.0
+#define TOP 255.0
 
 volatile float data;
 volatile float t = 0.0;
 
 void sinWave(void){
-    printf("int data[201] = {");
+    printf("int va[201] = {");
     for (int i = 0; i < 200; i++){                                          // interrupt 10 kHz - 200 sample
-        data = (M * 256 * sin(2.0*pi*F*t) + 256.0) / 2.0;                   // Phase 0
-        // data = (0.7*256 * sin(2.0*pi*F*t + 2.0*pi/3.0) + 256.0) / 2.0;   // Phase 1
-        // data = (0.7*256 * sin(2.0*pi*F*t + 4.0*pi/3.0) + 256.0) / 2.0;   // Phase 2
+        data = (M * TOP * sin(2.0*pi*F*t) + TOP) / 2.0;                   // Phase 0
+        //  data = (M * TOP * sin(2.0*pi*F*t + 2.0*pi/3.0) + TOP) / 2.0;   // Phase 1
+        //  data = (M * TOP * sin(2.0*pi*F*t + 4.0*pi/3.0) + TOP) / 2.0;   // Phase 2
         printf("%d, ", (int)round(data));        
         t += 0.0001; // 0 <= t <= 0.02
     }
